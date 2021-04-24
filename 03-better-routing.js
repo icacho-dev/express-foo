605e8f13009be03e1699006e01563240dfd37cb9
Isaac Cacho <maindesign@gmail.com>
2021-04-24 18:47:38 -0500

    adds better routing

---
+++ 03-better-routing.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const userRouter = require("./user-router")
+const app = express()
+const port = 3000
+
+app.use("/user", userRouter)
+
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`)
+})
---
+++ user-router.js
@@ -0,0 +1,5 @@
+function userRouteHandler(req, res) {
+  res.send('hello user handler')
+}
+
+module.exports = userRouteHandler;
